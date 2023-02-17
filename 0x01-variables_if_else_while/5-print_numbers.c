7edd312fc971c19385226ab6c027ef32f12e98e2
Tinaye Katemba <tinaye3434@gmail.com>
2023-02-17 12:29:48 -0800

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Print all single digits of base 10starting from 10
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
