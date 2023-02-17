6f4810f673bdefaafa0995856f3e44e50fb31b4f
Tinaye Katemba <tinaye3434@gmail.com>
2023-02-17 10:21:39 -0800

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Program that prints the alphabet in lower case
+ * Return: Always 0 (Succes)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch)
+	putchar(\n)
+	return (0)
+}
