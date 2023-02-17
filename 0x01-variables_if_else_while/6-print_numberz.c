6124a4eff425ad892843f809e5ce0ede278b71d2
Tinaye Katemba <tinaye3434@gmail.com>
2023-02-17 12:44:28 -0800

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Program that prints all single digit number starting from 0
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
