d6ba86d089c48ec696cd8db4a21c37d3912baf93
Tinaye Katemba <tinaye3434@gmail.com>
2023-02-17 12:18:12 -0800

    task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,9 +9,13 @@ int main(void)
 	char ch = 'a';
 
 	while (ch <= 'z')
-		if (ch != 'q' && ch != 'e')
+	{
+		if (ch != 'e' && ch != 'q')
+		{
 			putchar(ch);
+		}
 		ch++;
+	}
 	putchar('\n');
 	return (0);
 }
