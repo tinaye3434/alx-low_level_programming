b0baac19cc738fff2d9b277bcd92a030038a08e0
Tinaye Katemba <tinaye3434@gmail.com>
2023-02-19 15:46:45 -0800

    task 100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all possible different combinations of three digits
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i,j;
+
+	for (i = 48; i <= 58; i++)
+	{
+		for (j = 49; j <= 57; j++)
+		{
+			if (j > i)
+			{
+				putchar(i);
+				putchar(j);
+				if (i != 56 || j != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return(0)
+}
