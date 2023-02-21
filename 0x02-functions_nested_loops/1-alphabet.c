a8537a46de6b083cbb820731794d0ac67ba789e9
Tinaye Katemba <tinaye3434@gmail.com>
2023-02-21 09:56:18 -0800

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include <main.h>
+
+/**
+ * main - main entry for program
+ * print_alphabet - prints alphabet in small caps
+ * Return: 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch)
+	}
+	_putchar('\n');
+	return (0);
+}
+
+int main(void)
+{
+	print_alphabet()
+}
