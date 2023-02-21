1fb59de9b5340ad5c73b536a4d2721cbb168860e
Tinaye Katemba <tinaye3434@gmail.com>
2023-02-21 10:08:45 -0800

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,4 +1,4 @@
-#include <main.h>
+#include "main.h"
 
 /**
  * main - main entry for program
@@ -15,9 +15,4 @@ void print_alphabet(void)
 	}
 	_putchar('\n');
 	return (0);
-}
-
-int main(void)
-{
-	print_alphabet()
 }
