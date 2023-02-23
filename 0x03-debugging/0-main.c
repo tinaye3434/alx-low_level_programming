64aacdc686a6a7f1c80eb0a7a836732fd94f9b28
Tinaye Katemba <tinaye3434@gmail.com>
2023-02-23 14:40:54 -0800

    task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,13 +1,15 @@
 #include "main.h"
 
 /**
- * main - tests for positive or negative
- * Return: Always 0
- */
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+
 int main(void)
 {
-	int i = 0;
+	int i;
 
-	positve_or_negative(i);
+	i = 0;
+	positive_or_negative(i);
 	return (0);
 }
