22a41ebc4eb08678360a9bef5b312ef691387cea
Tinaye Katemba <tinaye3434@gmail.com>
2023-02-22 16:29:26 -0800

    task 5

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _abs - calculates the absolute value
+ * of integer
+ *
+ *@n: integer to be calculated
+ *
+ * Return: Absolute value
+ */
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (n * -1);
+	}
+}
