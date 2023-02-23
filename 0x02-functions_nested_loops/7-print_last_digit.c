0bb6dc49fa050972a772f30857c63c909fc43edb
Tinaye Katemba <tinaye3434@gmail.com>
2023-02-22 16:48:47 -0800

    task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -15,12 +15,12 @@ int print_last_digit(int n)
 	m = n % 10;
 	if (m < 0)
 	{
-		_putchar(-l + 48);
-		return (-l);
+		_putchar(-m + 48);
+		return (-m);
 	}
 	else
 	{
-		_putchar(l + 48);
-		return (l);
+		_putchar(m + 48);
+		return (m);
 	}
 }
