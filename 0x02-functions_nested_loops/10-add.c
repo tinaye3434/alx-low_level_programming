9c4d9975fa41572a6c3edd7354c4c15a2bbf19f5
Tinaye Katemba <tinaye3434@gmail.com>
2023-02-22 19:05:36 -0800

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ *@int: integers to be added
+ * Return: returmns result
+ */
+int add(int, int)
+{
+	int a, b, res;
+
+	res = a + b;
+	return (res);
+}
