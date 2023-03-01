bcd833edb0c27a06ebe9b43e271c9c320df17dff
Tinaye Katemba <tinaye3434@gmail.com>
2023-02-28 16:44:27 -0800

    7. Winning is only half of it. Having fun is the other half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -15,15 +15,17 @@ void puts_half(char *str)
 	{
 		len++;
 	}
+
 	j = (len / 2);
 
 	if ((len % 2) == 1)
 	{
 		j = ((len + 1) / 2);
 	}
+
 	for (i = j; str[i] != '\0'; i++)
 	{
-		_putchar(str[a]);
+		_putchar(str[i]);
 	}
 	_putchar('\n');
 }
